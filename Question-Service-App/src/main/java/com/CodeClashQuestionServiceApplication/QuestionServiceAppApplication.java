29d0507eabeda0cd5a8759ada59a27f526f6f0eb
raj-pandith <rajpanidth1945@gmail.com>
2025-10-18 19:05:52 +0530

    question service working fine for http://localhost:8081/questions?number=2&difficulty=Medium & return question with test cases expected output json

---
+++ Question-Service-App/src/main/java/com/CodeClashQuestionServiceApplication/QuestionServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.CodeClashQuestionServiceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceAppApplication.class, args);
+	}
+
+}
---
+++ Question-Service-App/src/main/java/com/CodeClashQuestionServiceApplication/controller/QuestionController.java
@@ -0,0 +1,23 @@
+package com.CodeClashQuestionServiceApplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.CodeClashQuestionServiceApplication.model.Question;
+import com.CodeClashQuestionServiceApplication.repository.QuestionRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping
+    public List<Question> getQuestions(@RequestParam int number,
+            @RequestParam String difficulty) {
+        return questionRepository.getQuestions(number, difficulty);
+    }
+}
